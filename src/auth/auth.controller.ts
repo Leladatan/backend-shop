33d556eae9f6a2246eb887436ddd2661aff2055a
Leladatan <vov4ik.samurai@yandex.ru>
2024-06-27 16:26:11 +0300

    ref: auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,16 +10,37 @@ import {
 } from '@nestjs/common';
 import { AuthService } from '@/auth/auth.service';
 import { AuthPasswordPayloadDto, AuthPayloadDto } from '@/auth/dto/auth.dto';
-import { TokensPayloadDto } from '@/tokens/dto/tokens.dto';
 import { FastifyReply } from 'fastify';
 import { GetCurrentUser } from '@/utils/decorators/get-current-user.decorator';
 import { Public } from '@/utils/decorators/public.decorator';
 import { RtGuard } from '@/utils/guards/rt.guard';
 import { User } from '@prisma/client';
+import { TokensPayloadDto } from '@/auth/tokens/dto/tokens.dto';
+import { TokensService } from '@/auth/tokens/tokens.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly tokensService: TokensService,
+  ) {}
+
+  @Get('refresh')
+  @UseGuards(RtGuard)
+  async refresh(
+    @GetCurrentUser('id') userId: number,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ): Promise<TokensPayloadDto> {
+    return this.tokensService.refreshTokens({ userId, res });
+  }
+
+  @Get('logout')
+  async logout(
+    @GetCurrentUser('id') userId: number,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ): Promise<void> {
+    return this.authService.logout({ userId, res });
+  }
 
   @Public()
   @Post('sign-in')
@@ -43,22 +64,5 @@ export class AuthController {
     @Body() payload: AuthPasswordPayloadDto,
   ): Promise<User> {
     return this.authService.changePassword({ userId, ...payload });
-  }
-
-  @Get('refresh')
-  @UseGuards(RtGuard)
-  async refresh(
-    @GetCurrentUser('id') userId: number,
-    @Res({ passthrough: true }) res: FastifyReply,
-  ): Promise<TokensPayloadDto> {
-    return this.authService.refresh({ userId, res });
-  }
-
-  @Get('logout')
-  async logout(
-    @GetCurrentUser('id') userId: number,
-    @Res({ passthrough: true }) res: FastifyReply,
-  ): Promise<void> {
-    return this.authService.logout({ userId, res });
   }
 }
